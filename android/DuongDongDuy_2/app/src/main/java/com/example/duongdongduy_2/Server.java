4b4f9b06ee71e665c734c9d0fa882867ab54a30b
TrungNguyen <63598647+TrungNguyen0204@users.noreply.github.com>
2023-06-13 15:51:44 +0700

    push

--- android/DuongDongDuy_2/app/src/main/java/com/example/duongdongduy_2/Server.java
+++
@@ -1,7 +0,0 @@
-package com.example.duongdongduy_2;
-
-public class Server {
-    public static String urlImage="http://192.168.43.66:8089/DuongDongDuy/hinhanh/";
-    public static String urlProduct="http://192.168.43.66:8089/DuongDongDuy/getProduct.php";
-    public static String urlBanner="http://192.168.43.66:8089/DuongDongDuy/getBanner.php";
-}
